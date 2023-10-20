5f81235cb065a4cc2ebaa0f7cb8634eb73047538
Martin DAVILA <aldntmi@gmail.com>
2023-10-20 15:37:25 +0200

    chore: Transform About js to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React, { useState, useEffect,useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Preloader from "./components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
-import About from './components/About/About.tsx';
+import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
   BrowserRouter as Router,
-  Route,
   Routes,
-  Navigate
+  Route,
+  Navigate,
 } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
   const [load, upadateLoad] = useState(true);
 
   useEffect(() => {
@@ -31,20 +33,23 @@ function App() {
   return (
     <Router>
       <Preloader load={load} />
+
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
+
         <ScrollToTop />
+
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/project" element={<Projects />} /> */}
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
+
         <Footer />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,56 +1,51 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import Particle from "../Particle";
-import Github from "./Github";
-import Techstack from "./Techstack";
-import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
-import Toolstack from "./Toolstack";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Particle from '../Particle';
+import Github from './Github';
+import Techstack from './Techstack';
+import Aboutcard from './AboutCard';
+import Toolstack from './Toolstack';
 
-// Define the types for props
-interface AboutProps {
-  laptopImg: string;
-}
+const laptopImg = require('../../Assets/about.png'); // Use require for image import
 
-// Create a functional component for the About section
-const About: React.FC<AboutProps> = ({ laptopImg }) => {
+
+interface AboutProps {}
+
+const About: React.FC<AboutProps> = () => {
   return (
     <Container fluid className="about-section">
       <Particle />
       <Container>
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
+        <Row style={{ justifyContent: 'center', padding: '10px' }}>
           <Col
             md={7}
             style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
+              justifyContent: 'center',
+              paddingTop: '30px',
+              paddingBottom: '50px',
             }}
           >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+            <h1 style={{ fontSize: '2.1em', paddingBottom: '20px' }}>
               Know Who <strong className="purple">I'M</strong>
             </h1>
             <Aboutcard />
           </Col>
           <Col
             md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            style={{ paddingTop: '120px', paddingBottom: '50px' }}
             className="about-img"
           >
             <img src={laptopImg} alt="about" className="img-fluid" />
           </Col>
         </Row>
         <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
+          Professional <strong className="purple">Skillset</strong>
         </h1>
-
         <Techstack />
-
         <h1 className="project-heading">
           <strong className="purple">Tools</strong> I use
         </h1>
         <Toolstack />
-
         <Github />
       </Container>
     </Container>
